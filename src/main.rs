1a97550c5ffc5f131b4727a9dc40d40a40b06990
Sven Stucki <git@svenstucki.ch>
2020-07-26 16:14:39 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    rtt_init_print!();
+    loop {
+        rprintln!("Hello, world!");
+    }
+}
